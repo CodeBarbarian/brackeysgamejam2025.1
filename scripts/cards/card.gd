f76072b2558502038c91a3a9a5481cb82c7ddfc0
Morten Haugstad <morten.haugstad@gmail.com>
2025-02-18 09:40:23 +0100

    Updates
    Added the Intial Card Class, and started working on the overall card mechanic.

--- scripts/cards/card.gd
+++ scripts/cards/card.gd
@@ -1,15 +1,41 @@
+@tool
 class_name Card extends Node2D
 
 @export var CardName: String = "Card Name"
 @export var CardDescription: String = "Card Description"
 @export var CardCost : int = 1
-@export var CardImage = Node2D
+@export var CardImage = Sprite2D
 
 @onready var CardNameLabel: Label = $CardName/CardNameLabel
 @onready var CardDescriptionLabel: Label = $CardDescription/CardDescriptionLabel
-@onready var CostLabel: Label = $CostDisplay/CostLabel
+@onready var CardCostLabel: Label = $CostDisplay/CostLabel
+
+func _update_graphics():
+	if CardNameLabel.get_text() != str(CardName):
+		CardNameLabel.set_text(str(CardName))
+	if CardDescriptionLabel.get_text()  != str(CardDescription):
+		CardDescriptionLabel.set_text(str(CardDescription))
+	if CardCostLabel.get_text() != str(CardCost):
+		CardCostLabel.set_text(str(CardCost))
+
+func set_card_name(name: String) -> void:
+	CardName = name
+	
+func set_card_cost(cost: int) -> void:
+	CardCost = cost
+	
+func set_card_description(description: String) -> void:
+	CardDescription = description
+
+func set_card_values(name: String, cost: int, description: String) -> void:
+	set_card_name(name)
+	set_card_cost(cost)
+	set_card_description(description)
+
+	_update_graphics()
 
 func _ready():
-	CardNameLabel.set_text(str(CardName))
-	CardDescriptionLabel.set_text(str(CardDescription))
-	CostLabel.set_text(str(CardCost))
+	set_card_values(CardName, CardCost, CardDescription)
+
+func _process(delta: float) -> void:
+	_update_graphics()
